416be7f4c8b016b7c0705fcce576afd6e0290c83
Alec <alec.arthur@adelaide.edu.au>
2024-11-27 15:10:02 +1030

    added skeleton setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Algorithms
+
+This repsitory contains the following algorithms along with tests using `SOME_TEST_LIBRARY`:
+
+ - A Linked List.
+ - A Binary Tree.
+ - A Graph using an Adjacency List.
+ - Depth First Search using recursion.
+ - Breadth First Search using a while loop. 
+
+## Setup
+
+    conda create -p ./.conda python=3.10 -y
+    conda activate ./.conda
+    pip install -r requirements.txt
+
+## Run the tests
+
+    pytest
---
+++ algorithms.py
@@ -0,0 +1,29 @@
+"""
+A Linked List.
+A Binary Tree.
+A Graph using an Adjacency List.
+Depth First Search using recursion.
+Breadth First Search using a while loop. 
+"""
+
+
+
+class Algorithms():
+    def linked_list(self):
+        pass
+
+    def binary_tree(self):
+        pass
+
+    def graph(self):
+        pass
+
+    def dfs(self):
+        pass
+
+    def bfs(self):
+        pass
+
+if __name__ == '__main__':
+    algorithms = Algorithms()
+    
